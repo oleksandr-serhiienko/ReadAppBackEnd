9959ba9126eced35f05a0ca4007ead95f895aab7
= <alex.ser.online@gnail.com>
2024-05-24 17:50:21 +0100

    deltelted not use libraries

--- src/main/java/com/example/demo/controller/WordController.java
+++ src/main/java/com/example/demo/controller/WordController.java
@@ -4,7 +4,6 @@ import com.example.demo.domain.Word;
 import com.example.demo.repository.WordRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/example/demo/domain/Word.java
+++ src/main/java/com/example/demo/domain/Word.java
@@ -1,6 +1,5 @@
 package com.example.demo.domain;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
